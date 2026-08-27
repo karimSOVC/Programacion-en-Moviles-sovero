f50859fc3e975144e065527b89e8799f42f5a27b
karimSOV <karim.sovero@tecsup.edu.pe>
2026-08-26 22:19:17 -0500

    Proyecto inicial con menu del carrito

---
+++ app/src/main/java/com/sovero/lab02carritokotlin_ia/Lab02CarritoKotlin_IA.kt
@@ -0,0 +1,7 @@
+package com.sovero.lab02carritokotlin_ia
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS- TIENDA TECSUP ")
+    println("=========================================")
+}
