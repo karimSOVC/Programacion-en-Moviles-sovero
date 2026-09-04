043f9a3a28f73726a9560a27df2cacf9d436b164
karimSOV <karim.sovero@tecsup.edu.pe>
2026-09-03 23:17:12 -0500

    commit 5 : se agrego fondo con degradado

--- Semana03/Lab03RegistroNotas/app/src/main/java/com/sovero/lab03registronotas/MainActivity.kt
+++ Semana03/Lab03RegistroNotas/app/src/main/java/com/sovero/lab03registronotas/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -28,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -63,6 +65,14 @@ fun PantallaNotas(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        MaterialTheme.colorScheme.primaryContainer,
+                        MaterialTheme.colorScheme.surface
+                    )
+                )
+            )
             .padding(16.dp)
     ) {
         Text(
