9430180ae6e6dd6cfc8dc77c5c3c3c9952a91289
karimSOVC <karimSovero@gmail.com>
2026-09-22 19:06:17 -0500

    implementa navegacion completa entre Home, List, Detail y Profile

--- Semana05/navegacion/app/src/main/java/com/tecsup/navegacion_sovero/MainActivity.kt
+++ Semana05/navegacion/app/src/main/java/com/tecsup/navegacion_sovero/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.tecsup.navegacion_sovero.navigation.AppNavigation
 import com.tecsup.navegacion_sovero.ui.theme.Navegacion_soveroTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Navegacion_soveroTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Navegacion_soveroTheme {
-        Greeting("Android")
     }
 }
---
+++ Semana05/navegacion/app/src/main/java/com/tecsup/navegacion_sovero/navigation/AppNavigation.kt
@@ -0,0 +1,44 @@
+package com.tecsup.navegacion_sovero.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.tecsup.navegacion_sovero.screens.DetailScreen
+import com.tecsup.navegacion_sovero.screens.HomeScreen
+import com.tecsup.navegacion_sovero.screens.ListScreen
+import com.tecsup.navegacion_sovero.screens.ProfileScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+        composable(Screen.List.route) {
+            ListScreen(navController)
+        }
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument(name = "itemId") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            DetailScreen(navController, itemId)
+        }
+    }
+}
